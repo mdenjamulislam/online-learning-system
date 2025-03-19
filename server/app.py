f3056eaced7c438f71815ad1c87120af29018d71
mdenjamulislam <mdenjamulislam527@gmail.com>
2025-03-19 17:14:30 +0600

    Create student table

--- server/app.py
+++ server/app.py
@@ -30,6 +30,15 @@ def init_db():
                     thum_url VARCHAR(255)
                 )
             ''')
+            # Create Students table
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS students (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    email VARCHAR(255) NOT NULL,
+                    password VARCHAR(255) NOT NULL
+                )
+            ''')
 
             connection.commit()
             return jsonify({'message': "Databse initilise successfully!"})
