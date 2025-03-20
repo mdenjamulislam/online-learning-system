8b4078fc32a90408023df1ba1e52fab438b07e5a
mdenjamulislam <mdenjamulislam527@gmail.com>
2025-03-20 14:26:37 +0600

    Initilize front-end

--- server/app.py
+++ server/app.py
@@ -36,7 +36,7 @@ def init_db():
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     name VARCHAR(255) NOT NULL,
                     email VARCHAR(255) NOT NULL,
-                    password VARCHAR(255) NOT NULL
+                    password VARCHAR(255) NOT NULL,
                 )
             ''')
 
